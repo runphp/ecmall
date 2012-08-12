277a220767f8585226c274928601dc7b742c2953
heui <runphp@qq.com>
2012-08-12 17:02:55 +0800

    忘记提交的js Signed-off-by: heui <runphp@qq.com>

---
+++ www/includes/libraries/javascript/jquery.spasticNav.js
@@ -0,0 +1,60 @@
+(function($) {
+
+	$.fn.spasticNav = function(options) {
+	
+		options = $.extend({
+			overlap : 20,
+			speed : 500,
+			reset : 1500,
+			color : '#0b2b61',
+			easing : 'easeOutExpo'
+		}, options);
+	
+		return this.each(function() {
+		
+		 	var nav = $(this),
+		 		currentPageItem = $('#selected', nav),
+		 		blob,
+		 		reset;
+		 		
+		 	$('<li id="blob"></li>').css({
+		 		width : currentPageItem.outerWidth(),
+		 		height : currentPageItem.outerHeight() + options.overlap,
+		 		left : currentPageItem.position().left,
+		 		top : currentPageItem.position().top - options.overlap / 2,
+		 		backgroundColor : options.color
+		 	}).appendTo(this);
+		 	
+		 	blob = $('#blob', nav);
+					 	
+			$('li:not(#blob)', nav).hover(function() {
+				// mouse over
+				clearTimeout(reset);
+				blob.animate(
+					{
+						left : $(this).position().left,
+						width : $(this).width()
+					},
+					{
+						duration : options.speed,
+						easing : options.easing,
+						queue : false
+					}
+				);
+			}, function() {
+				// mouse out	
+				reset = setTimeout(function() {
+					blob.animate({
+						width : currentPageItem.outerWidth(),
+						left : currentPageItem.position().left
+					}, options.speed)
+				}, options.reset);
+				
+			});
+		 
+		
+		}); // end each
+	
+	};
+
+})(jQuery);
